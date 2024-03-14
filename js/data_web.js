5fb19ee293de8ee1e6acbde4559704ac197698ae
misheldc <dmosorio@scl.edu.gt>
2024-03-14 05:57:48 +0000

    diseño terminado creo

---
+++ js/data_web.js
@@ -0,0 +1,30 @@
+let webs = [
+    {
+        nombre :"Galeria",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio:"https://github.com/mishelldc/galeria",
+        githubpage :"https://mishelldc.github.io/galeria/",
+        descripcion :" Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? ",
+    
+    },
+    {
+        nombre :"Galeria",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio:"https://github.com/mishelldc/galeria",
+        githubpage :"https://mishelldc.github.io/galeria/",
+        descripcion :" Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? ",
+    
+    },
+    {
+        nombre :"Galeria",
+        img :"https://foundations.projectpythia.org/_images/GitHub-logo.png",
+        repositorio:"https://github.com/mishelldc/galeria",
+        githubpage :"https://mishelldc.github.io/galeria/",
+        descripcion :" Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? ",
+    
+    }
+      
+
+];
+
+export {webs}
